8b916410f58b4000479365efa6ba3ec07567b7d6
Ricardo Hitalo Araujo Soares <ricardohitalo1@gmail.com>
2020-08-24 20:47:01 -0300

    update

---
+++ aww.c
@@ -0,0 +1,20 @@
+//DIZER QUEM OU O QUE VOU USAR.
+//BIBLIOTECA...
+
+#include<stdio.h> // aqui dentro tem todas as funções que iremos usar...
+#include<stdalign.h>
+#include<time.h>
+
+//VARIÁVEIS... QUE É AQUILO QUE VOU EXIGIR DO PC...
+int numero;
+
+// BLOCOS...
+// INICIA E TERMINA... 
+
+main () {
+// aqui é o bloco principal...
+    printf("Entre com um numero: ");
+    scanf("%d",&numero);
+
+    printf("Você colocou este numero: %d\n",numero);
+}
