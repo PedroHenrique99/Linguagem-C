8231c02c22ce2d62b95f2c20c3abfe988761f4eb
Pedro Henrique <99pedrohcs@gmail.com>
2020-09-11 10:12:16 -0300

    Create aula1exercicio3.c

---
+++ aula1exercicio3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero1, numero2, numero3;
+    float media;
+    numero1 = 5;
+    numero2 = 8;
+    numero3 = 12;
+    media = (numero1 + numero2 + numero3) / 3;
+    printf("\n A média dos números 5, 8 e 12 é: %.2f \n", media);
+    return 0;
+}
